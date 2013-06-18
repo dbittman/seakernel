44f096cd761ef1dad7b6d95c3a19fa7d8ba85a87
Daniel Bittman <danielbittman1@gmail.com>
2013-06-18 16:24:08 -0700

    cleaned up fork

--- kernel/tm/fork.c
+++ kernel/tm/fork.c
@@ -7,61 +7,61 @@
 
 unsigned running_processes = 0;
 
-void copy_task_struct(task_t *new, task_t *parent)
+void copy_task_struct(task_t *task, task_t *parent, char share_thread_data)
 {
-	new->parent = parent;
-	new->pid = add_atomic(&next_pid, 1)-1;
+	task->parent = parent;
+	task->pid = add_atomic(&next_pid, 1)-1;
 	
-	new->thread = (void *)kmalloc(sizeof(struct thread_shared_data));
-	new->thread->count = 1;
+	task->thread = (void *)kmalloc(sizeof(struct thread_shared_data));
+	task->thread->count = 1;
 	if(parent->thread->root) {
-		new->thread->root = parent->thread->root;
-		add_atomic(&new->thread->root->count, 1);
+		task->thread->root = parent->thread->root;
+		add_atomic(&task->thread->root->count, 1);
 	}
 	if(parent->thread->pwd) {
-		new->thread->pwd = parent->thread->pwd;
-		add_atomic(&new->thread->pwd->count, 1);
+		task->thread->pwd = parent->thread->pwd;
+		add_atomic(&task->thread->pwd->count, 1);
 	}
-	memcpy((void *)new->thread->signal_act, (void *)parent->thread->signal_act, 128 * 
+	memcpy((void *)task->thread->signal_act, (void *)parent->thread->signal_act, 128 * 
 		sizeof(struct sigaction));
-	new->thread->gid = parent->thread->gid;
-	new->thread->uid = parent->thread->uid;
-	new->thread->_uid = parent->thread->_uid;
-	new->thread->_gid = parent->thread->_gid;
-	mutex_create(&(new->thread->files_lock), 0);
-	new->thread->global_sig_mask = parent->thread->global_sig_mask;
+	task->thread->gid = parent->thread->gid;
+	task->thread->uid = parent->thread->uid;
+	task->thread->_uid = parent->thread->_uid;
+	task->thread->_gid = parent->thread->_gid;
+	mutex_create(&(task->thread->files_lock), 0);
+	task->thread->global_sig_mask = parent->thread->global_sig_mask;
 
-	new->magic = TASK_MAGIC;
-	new->tty = parent->tty;
-	new->sig_mask = parent->sig_mask;
+	task->magic = TASK_MAGIC;
+	task->tty = parent->tty;
+	task->sig_mask = parent->sig_mask;
 	
-	new->priority = parent->priority;
-	new->stack_end = parent->stack_end;
-	new->heap_end = parent->heap_end;
-	new->heap_start = parent->heap_start;
-	new->system = parent->system;
-	new->cmask = parent->cmask;
-	new->path_loc_start = parent->path_loc_start;
-	new->kernel_stack = kmalloc(KERN_STACK_SIZE+8);
+	task->priority = parent->priority;
+	task->stack_end = parent->stack_end;
+	task->heap_end = parent->heap_end;
+	task->heap_start = parent->heap_start;
+	task->system = parent->system;
+	task->cmask = parent->cmask;
+	task->path_loc_start = parent->path_loc_start;
+	task->kernel_stack = kmalloc(KERN_STACK_SIZE+8);
 	if(parent->mmf_priv_space) {
-		new->mmf_priv_space = (vma_t *)kmalloc(sizeof(vma_t));
-		memcpy(new->mmf_priv_space, parent->mmf_priv_space, sizeof(vma_t));
+		task->mmf_priv_space = (vma_t *)kmalloc(sizeof(vma_t));
+		memcpy(task->mmf_priv_space, parent->mmf_priv_space, sizeof(vma_t));
 	}
-	new->mmf_share_space = parent->mmf_share_space;
-	copy_mmf(parent, new);
+	task->mmf_share_space = parent->mmf_share_space;
+	copy_mmf(parent, task);
 	
 	/* This actually duplicates the handles... */
-	copy_file_handles(parent, new);
-	new->flags = TF_FORK;
-	mutex_create((mutex_t *)&new->exlock, MT_NOSCHED);
-	new->phys_mem_usage = parent->phys_mem_usage;
-	new->listnode = (void *)kmalloc(sizeof(struct llistnode));
-	new->activenode = (void *)kmalloc(sizeof(struct llistnode));
-	new->blocknode = (void *)kmalloc(sizeof(struct llistnode));
+	copy_file_handles(parent, task);
+	task->flags = TF_FORK;
+	mutex_create((mutex_t *)&task->exlock, MT_NOSCHED);
+	task->phys_mem_usage = parent->phys_mem_usage;
+	task->listnode = (void *)kmalloc(sizeof(struct llistnode));
+	task->activenode = (void *)kmalloc(sizeof(struct llistnode));
+	task->blocknode = (void *)kmalloc(sizeof(struct llistnode));
 }
 
 __attribute__((always_inline)) 
-inline static int engage_new_stack(task_t *new, task_t *parent)
+inline static int engage_new_stack(task_t *task, task_t *parent)
 {
 	assert(parent == current_task);
 	u32int ebp;
@@ -69,15 +69,15 @@ inline static int engage_new_stack(task_t *new, task_t *parent)
 	asm("mov %%esp, %0" : "=r"(esp));
 	asm("mov %%ebp, %0" : "=r"(ebp));
 	if(esp > TOP_TASK_MEM) {
-		new->esp=(esp-parent->kernel_stack) + new->kernel_stack;
-		new->ebp=(ebp-parent->kernel_stack) + new->kernel_stack;
-		new->sysregs = (parent->sysregs - parent->kernel_stack) + new->kernel_stack;
-		copy_update_stack(new->kernel_stack, parent->kernel_stack, KERN_STACK_SIZE);
+		task->esp=(esp-parent->kernel_stack) + task->kernel_stack;
+		task->ebp=(ebp-parent->kernel_stack) + task->kernel_stack;
+		task->sysregs = (parent->sysregs - parent->kernel_stack) + task->kernel_stack;
+		copy_update_stack(task->kernel_stack, parent->kernel_stack, KERN_STACK_SIZE);
 		return 1;
 	} else {
-		new->sysregs = parent->sysregs;
-		new->esp=esp;
-		new->ebp=ebp;
+		task->sysregs = parent->sysregs;
+		task->esp=esp;
+		task->ebp=ebp;
 		return 0;
 	}
 }
@@ -105,7 +105,7 @@ int do_fork(unsigned flags)
 	assert(running_processes < (unsigned)MAX_TASKS || MAX_TASKS == -1);
 	unsigned eip;
 	flush_pd();
-	task_t *new = (task_t *)kmalloc(sizeof(task_t));
+	task_t *task = (task_t *)kmalloc(sizeof(task_t));
 	page_dir_t *newspace;
 	if(flags & FORK_SHAREDIR)
 		newspace = vm_copy(current_task->pd);
@@ -113,30 +113,30 @@ int do_fork(unsigned flags)
 		newspace = vm_clone(current_task->pd, 0);
 	if(!newspace)
 	{
-		kfree((void *)new);
+		kfree((void *)task);
 		return -ENOMEM;
 	}
 	/* set the address space's entry for the current task.
 	 * this is a fast and easy way to store the "what task am I" data
 	 * that gets automatically updated when the scheduler switches
 	 * into a new address space */
-	newspace[PAGE_DIR_IDX(SMP_CUR_TASK/PAGE_SIZE)] = (unsigned)new;
+	newspace[PAGE_DIR_IDX(SMP_CUR_TASK/PAGE_SIZE)] = (unsigned)task;
 	/* Create the new task structure */
 	task_t *parent = (task_t *)current_task;
-	new->pd = newspace;
-	copy_task_struct(new, parent);
+	task->pd = newspace;
+	copy_task_struct(task, parent, 0);
 	add_atomic(&running_processes, 1);
 	/* Set the state as usleep temporarily, so that it doesn't accidentally run.
 	 * And then add it to the queue */
-	new->state = TASK_USLEEP;
-	tqueue_insert(primary_queue, (void *)new, new->listnode);
+	task->state = TASK_USLEEP;
+	tqueue_insert(primary_queue, (void *)task, task->listnode);
 	cpu_t *cpu = (cpu_t *)parent->cpu;
 #if CONFIG_SMP
 	cpu = fork_choose_cpu(parent);
 #endif
 	/* Copy the stack */
 	set_int(0);
-	engage_new_stack(new, parent);
+	engage_new_stack(task, parent);
 	/* Here we read the EIP of this exact location. The parent then sets the
 	 * eip of the child to this. On the reschedule for the child, it will 
 	 * start here as well. */
@@ -144,16 +144,16 @@ int do_fork(unsigned flags)
 	if((task_t *)current_task == parent)
 	{
 		/* These last things allow full execution of the task */
-		new->eip=eip;
-		new->state = TASK_RUNNING;
+		task->eip=eip;
+		task->state = TASK_RUNNING;
 		mutex_acquire(&cpu->lock);
-		new->cpu = cpu;
+		task->cpu = cpu;
 		add_atomic(&cpu->numtasks, 1);
-		tqueue_insert(cpu->active_queue, (void *)new, new->activenode);
+		tqueue_insert(cpu->active_queue, (void *)task, task->activenode);
 		mutex_release(&cpu->lock);
 		/* And unlock everything and reschedule */
 		__engage_idle();
-		return new->pid;
+		return task->pid;
 	}
 	return 0;
 }

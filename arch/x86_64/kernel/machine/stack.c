62fd535089995aee195ad8e27abc4fbee8c6542a
Daniel Bittman <danielbittman1@gmail.com>
2013-07-11 19:10:44 -0700

    rewrote stack.c for 64bit mode

--- arch/x86_64/kernel/machine/stack.c
+++ arch/x86_64/kernel/machine/stack.c
@@ -4,38 +4,38 @@
 extern unsigned int i_stack;
 /* This function's design is based off of JamesM's tutorials. 
  * Yes, I know its bad. But it works okay. */
-void move_stack(void *start, unsigned int sz)
+void move_stack(void *start, size_t sz)
 {
-	unsigned i;
-	for(i=(unsigned int)start+sz; i >= (unsigned int)start - sz*2;i -= 0x1000) {
+	u64int i;
+	for(i=((u64int)start)+sz; i >= (u64int)start - sz*2;i -= 0x1000) {
 		vm_map(i, pm_alloc_page(), PAGE_PRESENT | PAGE_WRITE | PAGE_USER, MAP_CRIT);
 		memset((void *)i, 0, 0x1000);
 	}
-	unsigned pd_addr;
-	u32int old_stack_pointer;
-	u32int old_base_pointer;
-	u32int offset = (u32int)start - i_stack;
-	u32int new_base_pointer, new_stack_pointer, tmp, *tmp2;
-	//asm("mov %%cr3, %0" : "=r" (pd_addr));
-	//asm("mov %0, %%cr3" : : "r" (pd_addr)); 
-	//asm("mov %%esp, %0" : "=r" (old_stack_pointer));
-	//asm("mov %%ebp, %0" : "=r" (old_base_pointer));
+	addr_t pd_addr;
+	addr_t old_stack_pointer;
+	addr_t old_base_pointer;
+	addr_t offset = (addr_t)start - i_stack;
+	addr_t new_base_pointer, new_stack_pointer, tmp, *tmp2;
+	asm("mov %%cr3, %0" : "=r" (pd_addr));
+	asm("mov %0, %%cr3" : : "r" (pd_addr)); 
+	asm("mov %%rsp, %0" : "=r" (old_stack_pointer));
+	asm("mov %%rbp, %0" : "=r" (old_base_pointer));
 	new_stack_pointer = old_stack_pointer + offset;
 	new_base_pointer  = old_base_pointer  + offset; 
 	memcpy((void*)new_stack_pointer, (void*)old_stack_pointer, i_stack-old_stack_pointer);
 	
-	for(i = (u32int)start; i > (u32int)start-sz; i -= 4)
+	for(i = (u64int)start; i > (u64int)start-sz; i -= 4)
 	{
-		tmp = * (u32int*)i;
+		tmp = * (u64int*)i;
 		if (( old_stack_pointer < tmp) && (tmp < i_stack))
 		{
 			tmp = tmp + offset;
-			tmp2 = (u32int*)i;
+			tmp2 = (u64int*)i;
 			*tmp2 = tmp;
 		}
 	}
-	//asm("mov %0, %%esp" : : "r" (new_stack_pointer));
-	//asm("mov %0, %%ebp" : : "r" (new_base_pointer));
+	asm("mov %0, %%rsp" : : "r" (new_stack_pointer));
+	asm("mov %0, %%rbp" : : "r" (new_base_pointer));
 }
 
 void setup_kernelstack()
@@ -46,6 +46,7 @@ void setup_kernelstack()
 
 void print_trace(unsigned int MaxFrames)
 {
+	/*
 	unsigned int * ebp = &MaxFrames - 2;
 	for(unsigned int frame = 0; frame < MaxFrames; ++frame)
 	{
@@ -57,21 +58,22 @@ void print_trace(unsigned int MaxFrames)
 		const char *name = elf64_lookup_symbol(eip, &kernel_elf);
 		if(name) kprintf("  <%x>  %s\n", eip, name);
 	}
+	*/
 }
 
 void copy_update_stack(addr_t new, addr_t old, unsigned length)
 {
 	memcpy((void *)new, (void *)old, length);
-	int offset=0;
+	u64int offset=0;
 	offset = new-old;
 	addr_t i;
-	for(i = (u32int)new+(length-4); i >= (u32int)new; i -= 4)
+	for(i = (addr_t)new+(length-8); i >= (u32int)new; i -= 8)
 	{
-		u32int tmp = * (u32int*)i;
+		u64int tmp = * (u64int*)i;
 		if (tmp >= old && tmp < old+length)
 		{
 			tmp = tmp + offset;
-			u32int *tmp2 = (u32int*)i;
+			u64int *tmp2 = (u64int*)i;
 			*tmp2 = tmp;
 		}
 	}

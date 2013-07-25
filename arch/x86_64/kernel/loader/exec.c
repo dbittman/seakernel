4c4c71edfe1213853c7be4dc75ae7cd2bb068729
Daniel Bittman <danielbittman1@gmail.com>
2013-07-25 12:42:48 -0700

    added execution for x86_64

--- arch/x86_64/kernel/loader/exec.c
+++ arch/x86_64/kernel/loader/exec.c
@@ -1,9 +1,8 @@
 #include <task.h>
 void arch_specific_exec_initializer(task_t *t, unsigned argc, addr_t eip)
 {
-#if 0
 	/* don't ya just love iret? */
-	t->sysregs->useresp = t->sysregs->ebp = STACK_LOCATION - STACK_ELEMENT_SIZE;
+	t->sysregs->useresp = t->sysregs->rbp = STACK_LOCATION - STACK_ELEMENT_SIZE;
 	*(unsigned *)t->sysregs->useresp = (unsigned)t->env;
 	t->sysregs->useresp -= STACK_ELEMENT_SIZE;
 	*(unsigned *)t->sysregs->useresp = (unsigned)t->argv;
@@ -12,5 +11,4 @@ void arch_specific_exec_initializer(task_t *t, unsigned argc, addr_t eip)
 	t->sysregs->useresp -= STACK_ELEMENT_SIZE;
 	
 	t->sysregs->eip = eip;
-#endif
 }

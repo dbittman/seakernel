10162f44a7f2503e6f3f392b7dc08a3cf20cad7d
Daniel Bittman <danielbittman1@gmail.com>
2013-08-09 17:12:48 -0700

    moved atomic.h to generic include

--- arch/x86/include/acpi-x86.h
+++
@@ -1,6 +0,0 @@
-#ifndef __X86_ACPI_H
-#define __X86_ACPI_H
-
-
-
-#endif
--- include/atomic.h
+++ include/atomic.h
@@ -1,6 +1,12 @@
-#include <config.h>
-#if CONFIG_ARCH == TYPE_ARCH_X86
-#include <atomic-x86.h>
-#elif CONFIG_ARCH == TYPE_ARCH_X86_64
-#include <atomic-x86_64.h>
+#ifndef __ATOMIC_H
+#define __ATOMIC_H
+
+#define bts_atomic(ptr, v) ((__sync_fetch_and_or(ptr, 1 << v) & (1 << v)) ? 1 : 0)
+#define btr_atomic(ptr, v) ((__sync_fetch_and_and(ptr, ~(1 << v)) & (1 << v)) ? 1 : 0)
+#define add_atomic(ptr, v) (__sync_add_and_fetch(ptr, v))
+#define sub_atomic(ptr, v) (__sync_sub_and_fetch(ptr, v))
+
+#define and_atomic(ptr, v) (__sync_and_and_fetch(ptr, v));
+#define or_atomic(ptr, v) (__sync_or_and_fetch(ptr, v));
+
 #endif

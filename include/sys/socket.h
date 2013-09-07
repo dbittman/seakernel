912803862221f52841d28eb96bb8ab4df90fa02c
Daniel Bittman <danielbittman1@gmail.com>
2013-09-07 14:54:02 -0700

    added socket stub files

---
+++ include/sys/socket.h
@@ -0,0 +1,11 @@
+#ifndef __SYS_SOCKET_H
+#define __SYS_SOCKET_H
+
+
+
+
+
+
+
+
+#endif
---
+++ kernel/dm/socket.c
@@ -0,0 +1,4 @@
+/* defines the basic functions for socket handling */
+#include <kernel.h>
+#include <fs.h>
+#include <sys/socket.h>

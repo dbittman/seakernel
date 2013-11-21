af75dbcb3afa21e8287775e8f2d84a0d91c378f1
Daniel Bittman <danielbittman1@gmail.com>
2013-11-20 22:22:51 -0800

    generalized arch/.../cpu.c

--- arch/x86/include/cpu-x86.h
+++ arch/x86/include/cpu-x86.h
@@ -2,13 +2,14 @@
 #define _CPU_X86_H
 
 typedef struct {
-    char manufacturer_string[13];
-    int max_basic_input_val;
-    int max_ext_input_val;
-    int features_ecx, features_edx;
-    char stepping, model, family, type; 
-    char cache_line_size, logical_processors, lapic_id;
-    char cpu_brand[49];
+	char manufacturer_string[13];
+	int max_basic_input_val;
+	int max_ext_input_val;
+	int features_ecx, features_edx;
+	int ext_features_ecx, ext_features_edx;
+	char stepping, model, family, type; 
+	char cache_line_size, logical_processors, lapic_id;
+	char cpu_brand[49];
 } cpuid_t;
 
 #include <cpu-x86_common.h>

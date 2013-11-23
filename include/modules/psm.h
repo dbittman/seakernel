7195b0f881a8603388eb3729820f0b8a754cc807
Daniel Bittman <danielbittman1@gmail.com>
2013-11-22 18:56:32 -0800

    PSM work

--- include/modules/psm.h
+++ include/modules/psm.h
@@ -2,8 +2,18 @@
 #define __MOD_PSM_H
 #include <config.h>
 #if CONFIG_MODULE_PSM
+#include <types.h>
+#include <dev.h>
 
+struct disk_info {
+	u64int_t length;
+	u64int_t num_sectors;
+	u64int_t sector_size;
+	
+};
 
+int register_disk_device(int identifier, dev_t dev, struct disk_info *info);
+int unregister_disk_device(int identifier, int psm_minor);
 
 #endif
 #endif

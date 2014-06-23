f63e8888378b9d82738ddd5ce9c0f244d208853a
Daniel Bittman <danielbittman1@gmail.com>
2014-06-23 15:04:24 -0700

    .

--- kernel/vsprintf.c
+++ kernel/vsprintf.c
@@ -232,7 +232,7 @@ int vsprintf(char *buf, const char *fmt, va_list args)
 	*str=0;
 	return 1;
 }
-
+#warning "CHANGE VSPRINTF TO VSNPRINTF"
 int snprintf(char *buf, size_t size, const char *fmt, ...)
 { 
 	va_list args;

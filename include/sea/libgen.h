d574bfbf0f2da232366baadf70edd41d84c655be
Daniel Bittman <danielbittman1@gmail.com>
2014-07-01 17:36:38 -0700

    added libgen.[ch]

---
+++ include/sea/libgen.h
@@ -0,0 +1,8 @@
+#ifndef __SEA_LIBGEN_H
+#define __SEA_LIBGEN_H
+
+char *dirname(char *path);
+char *basename(char *path);
+
+#endif
+
---
+++ library/klibc/libgen.c
@@ -0,0 +1,26 @@
+#include <sea/libgen.h>
+#include <sea/types.h>
+#include <sea/string.h>
+
+char *dirname(char *path)
+{
+	if(path == 0 || path[0] == 0)
+		return ".";
+	int length = strlen(path);
+	while((length>0) && path[length-1] == '/') {
+		if(length == 1)
+			return path;
+		path[(--length)] = 0;
+	}
+
+	while(length>0 && path[length-1] != '/') length--;
+
+	if(length == 1)
+		return "/";
+	else if(length == 0)
+		return ".";
+	else
+		path[length-1] = 0;
+	return path;
+}
+

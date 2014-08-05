045b6dc7b502cb351b5d1506a81f1d49ab4808d8
Daniel Bittman <danielbittman1@gmail.com>
2014-08-05 14:08:31 -0700

    cleaned up slab

--- include/sea/mm/slab.h
+++ include/sea/mm/slab.h
@@ -12,31 +12,31 @@ enum transfer_vals {
 	TO_FULL=2
 };
 
-typedef struct slab_header_s {
+struct slab {
 	unsigned magic;
 	unsigned id;
 	unsigned num_pages;
 	volatile unsigned obj_used;/* Ref count */
 	unsigned obj_num;
 	/* For usage in the slab lists */
-	volatile struct slab_header_s *next, *prev;
-	addr_t parent; /* Pointer to the slab cache that this slab is part of */
+	struct slab *next, *prev;
+	struct slab_cache *parent; /* Pointer to the slab cache that this slab is part of */
 	volatile unsigned short *stack;
 	unsigned short stack_arr[MAX_OBJ_ID];
 	struct valloc_region vreg;
-} slab_t;
+};
 
-typedef struct slab_cache_s {
-	slab_t *empty, *partial, *full;
+struct slab_cache {
+	struct slab *empty, *partial, *full;
 	/* Signed because an id of -1 means unused */
 	short id;
 	unsigned obj_size;
 	unsigned slab_count;
-} slab_cache_t;
+};
 
-#define NUM_SCACHES (PAGE_SIZE / sizeof(slab_cache_t))
-#define OBJ_SIZE(s) (((slab_cache_t *)(s->parent))->obj_size)
-#define FIRST_OBJ(s) ((addr_t)((addr_t)s+sizeof(slab_t)))
+#define NUM_SCACHES (PAGE_SIZE / sizeof(struct slab_cache))
+#define OBJ_SIZE(s) (((struct slab_cache *)(s->parent))->obj_size)
+#define FIRST_OBJ(s) ((addr_t)((addr_t)s+sizeof(struct slab)))
 
 #endif
 
--- kernel/mm/slab.c
+++ kernel/mm/slab.c
@@ -1,10 +1,6 @@
 /* slab.c (kernel/mm): Implementation on a SLAB allocation system.
  * Implementation is Copyright (c) 2010 Daniel Bittman. 
  * 
- * Note: This is not a pure SLAB allocator! It is simply based off the SLAB design 
- * heavily, with only minor modifications to allow easier implementation.
- * First implementation completed October 13th, 2010
- * 
  * Diagram of memory layout:
  * | = page bound
  * start                      --Slabs--->                      end
@@ -15,20 +11,18 @@
  */ 
 #include <sea/kernel.h>
 #include <sea/mm/vmm.h>
-#include <sea/tm/process.h>
 #include <sea/cpu/atomic.h>
 #include <sea/mm/slab.h>
 #include <sea/mm/valloc.h>
-#define SLAB_NUM_INDEX 120
 
 static addr_t slab_start=0, slab_end=0;
 static struct valloc slab_valloc;
 
-static slab_cache_t *scache_list[NUM_SCACHES];
+static struct slab_cache *scache_list[NUM_SCACHES];
 static unsigned pages_used=0;
 
 static unsigned num_slab=0, num_scache=0;
-static void release_slab(slab_t *slab);
+static void release_slab(struct slab *slab);
 static mutex_t scache_lock;
 
 static struct valloc_region *alloc_slab(struct valloc_region *vr, unsigned np)
@@ -41,12 +35,9 @@ static struct valloc_region *alloc_slab(struct valloc_region *vr, unsigned np)
 	return vr;
 }
 
-static void free_slab(slab_t *slab)
+static void free_slab(struct slab *slab)
 {
 	unsigned num_pages = slab->num_pages;
-#ifdef SLAB_DEBUG
-	printk(0, "[slab]: free slab %x - %x\n", (addr_t)slab, (addr_t)slab + num_pages * PAGE_SIZE);
-#endif
 	assert(slab->magic == SLAB_MAGIC);
 	assert(slab);
 	pages_used -= slab->num_pages;
@@ -61,32 +52,31 @@ static void free_slab(slab_t *slab)
 	sub_atomic(&num_slab, 1);
 }
 
-static slab_cache_t *get_empty_scache(int size)
+static struct slab_cache *get_empty_scache(int size)
 {
 	assert(size);
 	unsigned i=0;
 	mutex_acquire(&scache_lock);
 	while(i < NUM_SCACHES)
 	{
-		if(((slab_cache_t *)(scache_list[i]))->id == -1)
+		if(scache_list[i]->id == -1)
 			break;
 		i++;
 	}
-	/* Normally, we would panic. However, the allocator has a 
-	 * condition which might allow us to get around this. */
 	if(i == NUM_SCACHES) {
+		/* couldn't find one */
 		mutex_release(&scache_lock);
 		return 0;
 	}
-	memset(((slab_cache_t *)(scache_list[i])), 0, sizeof(slab_cache_t));
-	((slab_cache_t *)(scache_list[i]))->id = i;
-	((slab_cache_t *)(scache_list[i]))->obj_size = size;
+	memset(scache_list[i], 0, sizeof(struct slab_cache));
+	scache_list[i]->id = i;
+	scache_list[i]->obj_size = size;
 	add_atomic(&num_scache, 1);
 	mutex_release(&scache_lock);
 	return scache_list[i];
 }
 
-static void release_scache(slab_cache_t *sc)
+static void release_scache(struct slab_cache *sc)
 {
 	assert(sc);
 	assert(!sc->full && !sc->partial && !sc->empty);
@@ -100,12 +90,12 @@ static void release_scache(slab_cache_t *sc)
  * from it's old list though. WARNING: Before calling this, remove the 
  * slab from the current list first, Otherwise, we might corrupt the 
  * current list */
-static int add_slab_to_list(slab_t *slab, char to)
+static int add_slab_to_list(struct slab *slab, char to)
 {
 	assert(slab && slab->magic == SLAB_MAGIC);
 	assert(slab->next==0 && slab->prev==0);
-	slab_t **list=0;
-	slab_cache_t *sc = (slab_cache_t *)(slab->parent);
+	struct slab **list=0;
+	struct slab_cache *sc = slab->parent;
 	assert(sc);
 	switch(to)
 	{
@@ -124,42 +114,40 @@ static int add_slab_to_list(slab_t *slab, char to)
 			break;
 	}
 	assert(list);
-	/* Woah, double pointers! While confusing, they make 
+	/* Woah, double pointers! While annoying, they make 
 	 * things _much_ easier to handle. */
-	slab_t *old = *list;
-	if(old) assert(old->magic == SLAB_MAGIC);
+	struct slab *old = *list;
 	*list = slab;
-	if(old) old->prev = slab;
+	if(old)
+		old->prev = slab;
 	slab->next = old;
 	slab->prev=0;
 	return 0;
 }
 
-static void remove_slab_list(slab_t *slab)
+static void remove_slab_list(struct slab *slab)
 {
 	assert(slab && slab->magic == SLAB_MAGIC);
-	slab_cache_t *sc = (slab_cache_t *)(slab->parent);
+	struct slab_cache *sc = slab->parent;
 	assert(sc);
 	if(slab->prev)
 		slab->prev->next=slab->next;
 	if(slab->next)
 		slab->next->prev = slab->prev;
 	/* If the lists of the cache point to this slab, we need to clear that... */
-	slab_t **list=0;
+	struct slab **list=0;
 	if(sc->empty == slab)
 		list = &sc->empty;
 	else if(sc->partial == slab)
 		list = &sc->partial;
 	else if(sc->full == slab)
 		list = &sc->full;
-	if(list) {
-		*list = (slab_t *)slab->next;
-		if(!*list) assert(!slab->prev);
-	}
+	if(list)
+		*list = (struct slab *)slab->next;
 	slab->prev = slab->next=0;
 }
 
-static slab_t *create_slab(slab_cache_t *sc, int num_pages)
+static struct slab *create_slab(struct slab_cache *sc, int num_pages)
 {
 	assert(sc && num_pages);
 	addr_t addr=0;
@@ -174,16 +162,16 @@ static slab_t *create_slab(slab_cache_t *sc, int num_pages)
 		if(!mm_vm_get_map(j, 0, 0))
 			mm_vm_map(j, mm_alloc_physical_page(), PAGE_PRESENT, MAP_CRIT);
 	}
-	slab_t *slab = (slab_t *)addr;
+	struct slab *slab = (struct slab *)addr;
 	assert(slab->magic != SLAB_MAGIC);
-	memset(slab, 0, sizeof(slab_t));
+	memset(slab, 0, sizeof(struct slab));
 	slab->magic = SLAB_MAGIC;
-	slab->parent = (addr_t)sc;
+	slab->parent = sc;
 	slab->num_pages = num_pages;
 	memcpy(&slab->vreg, &vreg, sizeof(struct valloc_region));
-	slab->obj_num = ((num_pages*PAGE_SIZE)-sizeof(slab_t)) / sc->obj_size;
+	slab->obj_num = ((num_pages*PAGE_SIZE)-sizeof(struct slab)) / sc->obj_size;
 	assert(slab->obj_num > 0);
-	assert((int)sizeof(slab_t) < (num_pages * PAGE_SIZE));
+	assert((int)sizeof(struct slab) < (num_pages * PAGE_SIZE));
 	if(slab->obj_num > MAX_OBJ_ID)
 		slab->obj_num = MAX_OBJ_ID;
 	slab->stack = (unsigned short *)slab->stack_arr;
@@ -197,17 +185,17 @@ static slab_t *create_slab(slab_cache_t *sc, int num_pages)
 	unsigned short *tmp = (unsigned short *)slab->stack;
 	while(i < MAX_OBJ_ID)
 	{
-		(*tmp) = 0xFFFF;
+		*tmp = 0xFFFF;
 		i++;
 		tmp++;
 	}
 	return slab;
 }
 
-static addr_t do_alloc_object(slab_t *slab)
+static addr_t do_alloc_object(struct slab *slab)
 {
 	assert(slab && slab->magic == SLAB_MAGIC);
-	slab_cache_t *sc = (slab_cache_t *)(slab->parent);
+	struct slab_cache *sc = slab->parent;
 	assert(sc);
 	if(slab->obj_used >= slab->obj_num)
 		panic(PANIC_MEM | PANIC_NOSYNC, 
@@ -217,12 +205,13 @@ static addr_t do_alloc_object(slab_t *slab)
 	unsigned short obj = *(--slab->stack);
 	assert(obj != 0xFFFF);
 	addr_t obj_addr = FIRST_OBJ(slab) + OBJ_SIZE(slab)*obj;
-	assert((obj_addr > (addr_t)slab) && (((obj_addr+sc->obj_size)-(addr_t)slab) <= slab->num_pages*PAGE_SIZE));
+	assert((obj_addr > (addr_t)slab)
+			&& (((obj_addr+sc->obj_size)-(addr_t)slab) <= slab->num_pages*PAGE_SIZE));
 	slab->obj_used++;
 	return obj_addr;
 }
 
-static addr_t alloc_object(slab_t *slab)
+static addr_t alloc_object(struct slab *slab)
 {
 	addr_t ret = do_alloc_object(slab);
 	assert(slab->obj_used <= slab->obj_num);
@@ -239,7 +228,7 @@ static addr_t alloc_object(slab_t *slab)
 	return ret;
 }
 
-static unsigned do_release_object(slab_t *slab, int obj)
+static unsigned do_release_object(struct slab *slab, int obj)
 {
 	assert(slab && slab->magic == SLAB_MAGIC);
 	assert(slab->obj_used);
@@ -251,7 +240,7 @@ static unsigned do_release_object(slab_t *slab, int obj)
 	return ret;
 }
 
-static void release_object(slab_t *slab, int obj)
+static void release_object(struct slab *slab, int obj)
 {
 	unsigned res = do_release_object(slab, obj);
 	if(!res)
@@ -259,21 +248,17 @@ static void release_object(slab_t *slab, int obj)
 		/* We must move it to the empty list or destroy it */
 		remove_slab_list(slab);
 		release_slab(slab);
-	} else
-	{
+	} else if((res+1) >= (slab->obj_num)) {
 		/* If it was in the full, then we must switch lists */
-		if((res+1) >= (slab->obj_num))
-		{
-			remove_slab_list(slab);
-			add_slab_to_list(slab, TO_PARTIAL);
-		}
+		remove_slab_list(slab);
+		add_slab_to_list(slab, TO_PARTIAL);
 	}
 }
 
-static void do_release_slab(slab_t *slab)
+static void do_release_slab(struct slab *slab)
 {
 	assert(slab && slab->magic == SLAB_MAGIC);
-	slab_cache_t *sc = (slab_cache_t *)(slab->parent);
+	struct slab_cache *sc = slab->parent;
 	assert(sc);
 	if(slab->obj_used)
 		panic(PANIC_MEM | PANIC_NOSYNC, "Tried to release used slab");
@@ -282,9 +267,9 @@ static void do_release_slab(slab_t *slab)
 	free_slab(slab);
 }
 
-static void release_slab(slab_t *slab)
+static void release_slab(struct slab *slab)
 {
-	slab_cache_t *sc = (slab_cache_t *)(slab->parent);
+	struct slab_cache *sc = slab->parent;
 	assert(sc);
 	do_release_slab(slab);
 	if(!sc->slab_count)
@@ -303,11 +288,11 @@ unsigned __mm_slab_init(addr_t start, addr_t end)
 	unsigned i;
 	for(i=0;i<NUM_SCACHES;i++)
 	{
-		scache_list[i] = (slab_cache_t *)(start + sizeof(slab_cache_t)*i);
+		scache_list[i] = (struct slab_cache *)(start + sizeof(struct slab_cache)*i);
 		scache_list[i]->id=-1;
 	}
 	printk(1, "done\n");
-	pages_used = SLAB_NUM_INDEX+1;
+	pages_used = slab_valloc.nindex+1;
 	mutex_create(&scache_lock, 0);
 	return 0;
 }
@@ -327,24 +312,26 @@ unsigned __mm_slab_init(addr_t start, addr_t end)
 
 static unsigned slab_size(int sz)
 {
-	unsigned s = (sz * MAX_OBJ_ID) + sizeof(slab_t);
+	unsigned s = (sz * MAX_OBJ_ID) + sizeof(struct slab);
 	if(s > (PAGE_SIZE * 128))
 		s = PAGE_SIZE * 128;
-	if(s < (unsigned)sz * 2) s = sz * 2;
+	if(s < (unsigned)sz * 2)
+		s = sz * 2;
 	s = (s&PAGE_MASK) + PAGE_SIZE;
 	return s;
 }
 
-static slab_t *find_usable_slab(unsigned size, int allow_range)
+static struct slab *find_usable_slab(unsigned size, int allow_range)
 {
 	unsigned i;
 	int perfect_fit=0;
-	slab_cache_t *new_sc;
-	slab_t *slab;
-	slab_cache_t *sc=0;
+	struct slab_cache *new_sc;
+	struct slab *slab;
+	struct slab_cache *sc=0;
 	look_again_perfect:
 	i = perfect_fit;
-	if(i) i++;
+	if(i)
+		i++;
 	for(;i<NUM_SCACHES;i++)
 	{
 		if(scache_list[i]->id != -1 
@@ -431,28 +418,31 @@ void __slab_do_kfree_aligned(addr_t a)
 	valloc_deallocate(&slab_valloc, &vr);
 }
 
+/* aligned allocations are handled seperately. Non-aligned ones
+ * are forced to be non-aligned, since free checks whether the
+ * pointer passed to it is aligned and handles that differently.
+ */
 addr_t __mm_do_kmalloc_slab(size_t sz, char align)
 {
 	if(align)
 		return __slab_do_kmalloc_aligned(sz);
-	if(sz < 32) sz=32;
+	/* fix size. minimum size is 32 bytes, and we
+	 * need a few extra bits for the non-alignment
+	 * forcing */
+	if(sz < 32)
+		sz = 32;
 	sz += (sizeof(addr_t) * 2);
-	slab_t *slab=0, *old_slab=0;
-	while(!(slab = find_usable_slab(sz, 1)));
+	struct slab *slab = find_usable_slab(sz, 1);
 	assert(slab && slab->magic == SLAB_MAGIC);
 	if(slab->obj_used >= slab->obj_num)
 		panic(PANIC_MEM | PANIC_NOSYNC, 
 			"Attemping to allocate from full slab: %d:%d\n", 
 			slab->obj_used, slab->obj_num);
 	addr_t addr = alloc_object(slab);
-	if(align && !((addr&PAGE_MASK) == addr))
-		panic(PANIC_MEM | PANIC_NOSYNC, 
-			"slab allocation of aligned data failed! (returned %x)", addr);
+	/* force non-alignment */
 	if(((addr + sizeof(addr_t *)) & PAGE_MASK) == (addr + sizeof(addr_t *)))
-	{
-		/* force non-alignment */
 		addr += sizeof(addr_t);
-	}
+	/* save a pointer to the slab that this is from for easy recovery in free */
 	*(addr_t *)(addr) = (addr_t)slab;
 	addr += sizeof(addr_t);
 	assert((addr & PAGE_MASK) != addr);
@@ -468,20 +458,20 @@ void __mm_do_kfree_slab(void *ptr)
 			current_task ? current_task->system : 0);
 		return;
 	}
-	slab_t *slab=0;
+	struct slab *slab=0;
 	if(((addr_t)ptr&PAGE_MASK) == (addr_t)ptr) {
+		/* the pointer is page aligned. handle it specially */
 		__slab_do_kfree_aligned((addr_t)ptr);
 		return;
 	}
 	else {
-		slab = (slab_t *)*(addr_t *)((addr_t)ptr - sizeof(addr_t));
+		slab = (struct slab *)*(addr_t *)((addr_t)ptr - sizeof(addr_t));
  	}
-	if(!slab)
-		panic(PANIC_MEM | PANIC_NOSYNC, "Kfree got invalid address in task %d, system=%d (%x)", current_task->pid, current_task->system, ptr);
-	assert(slab->magic == SLAB_MAGIC);
-	slab_cache_t *sc = (slab_cache_t *)slab->parent;
+	assert(slab && slab->magic == SLAB_MAGIC);
+	struct slab_cache *sc = slab->parent;
 	unsigned obj;
 	obj = ((addr_t)ptr-FIRST_OBJ(slab)) / sc->obj_size;
 	assert(obj < slab->obj_num);
 	release_object(slab, obj);
 }
+

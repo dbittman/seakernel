9c9de71116633e6852cb4311dbd5c78438168287
Daniel Bittman <danielbittman1@gmail.com>
2015-04-15 14:58:51 -0700

    Update ex.s

--- drivers/shiv/ex.s
+++ drivers/shiv/ex.s
@@ -1,18 +1,20 @@
 [BITS 64]
 [ORG 0x2100]
 
-mov esp, 0x2500
+mov esp, 0x2500 ; we'll need a stack for the interrupt handler
 xor rax, rax
-mov rcx, 12345678abcdefh
-lidt [idtr]
-pushfq
+mov rcx, 12345678abcdefh ; fill this in with the proper, determined value
+lidt [idtr] ; load the interrupt table
+pushfq ; get the flags
 pop rax
-or rax, 100000000b
+or rax, 100000000b ; set TF
 push rax
-popf
-mov cr3, rcx
+popf ; set the flags
+mov cr3, rcx ; change address spaces
+; <--- TF triggers interrupt here
 hlt
 
+; IDT table contains a null entry for interrupt 0, and the entry for interrupt 1
 ALIGN 16
 idtr:
 	dw 0x100 - 1
@@ -29,7 +31,7 @@ idt_start:
 	dd 0
 
 	; int 1
-	dw 0x3100 ; offset low bits
+	dw 0x3100 ; offset low bits (some location to jump to at interrupt)
 	dw 0x8    ; CS selector
 	db 0      ; zero
 	db 0x8e   ; type & attributes (interrupt, present)
@@ -38,4 +40,3 @@ idt_start:
 	dd 0      ; zero
 
 idt_end:
-

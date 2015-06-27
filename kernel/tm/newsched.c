6c55577eb3a086ff8ed2bf14db8fe1c6d8acb17d
Daniel Bittman <danielbittman1@gmail.com>
2015-06-27 00:10:54 -0700

    added sched stub

---
+++ kernel/tm/newsched.c
@@ -0,0 +1,28 @@
+#include <sea/tm/thread.h>
+#include <sea/tm/process.h>
+
+static struct thread *get_next_thread()
+{
+
+}
+
+static void prepare_schedule()
+{
+
+}
+
+static void finish_schedule()
+{
+
+}
+
+static void balance_execs()
+{
+
+}
+
+void tm_schedule()
+{
+
+}
+

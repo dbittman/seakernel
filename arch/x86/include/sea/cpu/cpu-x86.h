b7024f6ab114c3419c590ee748eb8f337509c247
Daniel Bittman <danielbittman1@gmail.com>
2015-09-10 23:11:48 -0700

    Remove x86 subdirectory.

--- arch/x86/include/sea/cpu/cpu-x86.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CPU_X86_H
-#define _CPU_X86_H
-
-#include <sea/cpu/cpu-x86_common.h>
-
-#endif
--- arch/x86/include/sea/cpu/isr-x86.h
+++
@@ -1,8 +0,0 @@
-#ifndef ISR_x86_H
-#define ISR_x86_H
-#include <sea/types.h>
-#include <sea/cpu/registers-x86.h>
-
-#include <sea/cpu/isr-x86_common.h>
-
-#endif

6b948c215151cd64fa13f6175054ddba21dea558
Daniel Bittman <danielbittman1@gmail.com>
2015-10-21 18:59:17 -0700

    Remove old hash code.

--- include/sea/lib/hash-simple.h
+++ include/sea/lib/hash-simple.h
@@ -1,15 +1,6 @@
 #ifndef __SEA_LIB_HASH_SIMPLE_H
 #define __SEA_LIB_HASH_SIMPLE_H
 
-#include <sea/lib/hash.h>
-
-static inline struct hash_table *hash_table_create_default(struct hash_table *h, int count)
-{
-	h = hash_table_create(h, 0, HASH_TYPE_CHAIN);
-	hash_table_resize(h, HASH_RESIZE_MODE_IGNORE, count);
-	hash_table_specify_function(h, HASH_FUNCTION_DEFAULT);
-	return h;
-}
 
 #endif
 
